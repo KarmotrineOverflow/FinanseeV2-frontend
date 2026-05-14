b599988858056c158bed2149ad3d5d646de0a871
Elfie Marvin John Campit <elfiemarvinjohncampit@gmail.com>
2026-05-14 16:02:59 +0800

    Added structure to SignIn page

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,8 +1,27 @@
 export default function SignIn() {
 
     return (
-        <div>
-            
+        <div className="flex flex-col justify-between relative">
+
+            {/* Spans for rendering the colored corner designs */}
+            <span className="absolute top-0 right-0 overflow-hidden">
+                
+            </span>
+            <span className="absolute top-0 right-0 overflow-hidden">
+
+            </span>
+
+            <header>
+                <img src="/finansee-logo-light.png" />
+            </header>
+
+            <main>
+                
+            </main>
+
+            <footer>
+
+            </footer>
         </div>
     )
 }
