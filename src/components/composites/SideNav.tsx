449e7e10f4f18c1d20169f8cc7f347c277c00121
Elfie Marvin John Campit <elfiemarvinjohncampit@gmail.com>
2026-06-26 16:10:24 +0800

    Small fixes and added settings and log out buttons to side nav

--- src/components/composites/SideNav.tsx
+++ src/components/composites/SideNav.tsx
@@ -1,10 +1,10 @@
-import { SquareChevronLeftIcon, SquareChevronRightIcon } from "lucide-react";
+import { LogOut, Settings, SquareChevronLeftIcon, SquareChevronRightIcon } from "lucide-react";
 
 export default function SideNav({ children, isOpen, toggleOpen } : { children: React.ReactNode, isOpen: boolean, toggleOpen: () => void }) {    
 
     return (
         <aside className={(isOpen) ? openSideNavStyle() : closedSideNavStyle()}>
-            <nav>
+            <nav className="h-full grid grid-cols-1 grid-rows-[max-content_1fr]">
                 <span className="mb-2 p-2 flex justify-between">
                     {isOpen && <img src='/finansee_logo_2.png' className="w-36"/>}
                     <button className="cursor-pointer"
@@ -16,10 +16,33 @@ export default function SideNav({ children, isOpen, toggleOpen } : { children: R
                                 : <SquareChevronRightIcon size={36} strokeWidth={1.5} color="white"/>
                         }                        
                     </button>
-                </span>
-                <ul>                    
+                </span>                 
+
+                {/* Nav for main pages */} 
+                <ul>
                     { children }
-                </ul>                
+                </ul>
+
+                {/* Static nav for logout and settings. This will always be shown at the bottom of the nav */}
+                <ul>
+                    <hr className="text-white" />
+                    <li className="w-full">
+                        <button
+                        className={`px-4 w-full p-2 flex align-middle gap-2 text-white cursor-pointer hover:bg-[#FFBF69]`}
+                        >
+                            <Settings />
+                            {isOpen && <p className="font-semibold text-[16px]">Settings</p>}                
+                        </button>
+                    </li>
+                    <li className="w-full">
+                        <button
+                        className={`px-4 w-full p-2 flex align-middle gap-2 text-white cursor-pointer hover:bg-[#FFBF69]`}
+                        >
+                            <LogOut />
+                            {isOpen && <p className="font-semibold text-[16px]">Log Out</p>}                
+                        </button>
+                    </li>
+                </ul>                                  
             </nav>
         </aside>
     )
@@ -27,9 +50,9 @@ export default function SideNav({ children, isOpen, toggleOpen } : { children: R
 
 function openSideNavStyle() {
 
-    return [
-        "sm:min-w-64",
-        "h-screen",
+    return [            
+        "sm:min-w-56",
+        "h-full",
         "bg-[#FF9F1C]"
     ].join(' ')
 }
@@ -38,7 +61,7 @@ function closedSideNavStyle() {
 
     return [
         "w-fit",
-        "h-screen",
+        "h-full",
         "bg-[#FF9F1C]"
     ].join(' ')
 }
